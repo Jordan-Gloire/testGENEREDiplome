ebc0725443d92dd499673dba8ea86261e5e2e440
Jordan Gloire <132207668+Jordan-Gloire@users.noreply.github.com>
2024-02-27 17:15:19 +0100

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,6 @@
+const montoggleMenu = document.getElementById('menutoggle')
+const mespages = document.getElementById('mespages')
+montoggleMenu.addEventListener('click', () => {
+    mespages.classList.toggle('viewpages')
+})
+
